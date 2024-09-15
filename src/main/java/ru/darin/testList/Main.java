9b188d383d74be0907fdc4eabd19514b5271e922
sergei_darin <swd86@mail.ru>
2024-09-15 20:31:58 +0400

    initial commit

---
+++ src/main/java/ru/darin/testList/Main.java
@@ -0,0 +1,27 @@
+package ru.darin.testList;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer>list = new ArrayList<>();
+        list.add(1);
+
+        List<Integer>list1 = new ArrayList<>();
+        list1.add(1);
+
+        String str = "abcaabc";
+        char [] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i++){
+//            System.out.println(chars[i]);
+//            for (int j = i + 1; j < chars.length; j ++){
+//                if (chars[i]==chars[j]){
+//                    System.out.println(chars[i]);
+//                }
+//            }
+        }
+    }
+}
