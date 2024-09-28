23f19a9631e0a11892d4958918c4b6113535e562
sergei_darin <swd86@mail.ru>
2024-09-28 22:35:10 +0400

    Добавлен класс MultiThreading. Пример работы с многопоточностью - ключевое слово volatile

---
+++ src/main/java/ru/darin/testList/MultiThreading.java
@@ -0,0 +1,44 @@
+package ru.darin.testList;
+
+import java.util.Scanner;
+
+public class MultiThreading {
+
+    public static void main(String[] args) {
+        ThreadClass threadClass = new ThreadClass();
+        threadClass.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        threadClass.shutDown();
+    }
+}
+
+class Runner implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello from Runner");
+    }
+}
+
+class ThreadClass extends Thread {
+
+    public volatile boolean running = true;
+
+    public void run() {
+        while (running) {
+            System.out.println("Hello from my Thread");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void shutDown() {
+        this.running = false;
+    }
+
+}
