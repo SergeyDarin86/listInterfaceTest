4af4a06a120ee91a8e739263e61a3d0c51c86f0b
sergei_darin <swd86@mail.ru>
2024-10-22 13:24:31 +0400

    Пример использования класса ReentrantLock

---
+++ src/main/java/ru/darin/testList/ReentrantLockExample.java
@@ -0,0 +1,55 @@
+package ru.darin.testList;
+
+// Класс ReentrantLock нужен для синхронизации потоков, но у него есть свои плюсы
+// его иногда удобнее использовать, чем ключевое слово "synchronized"
+// иногда можно столкнуться с deadlock, используя synchronized - это взаимная блокировка потоками друг друга
+// Он реализует интерфейс Lock
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockExample {
+    public static void main(String[] args) throws InterruptedException {
+        Task task = new Task();
+
+        Thread thread1 = new Thread(task::firstThread);
+
+        Thread thread2 = new Thread(task::secondThread);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        task.showCounter();
+    }
+}
+
+class Task {
+    private int counter;
+
+    private Lock lock = new ReentrantLock();
+
+    private void increment() {
+        for (int i = 0; i < 10000; i++) {
+            counter++;
+        }
+    }
+
+    public void firstThread() {
+        lock.lock();
+        increment();
+        lock.unlock();
+    }
+
+    public void secondThread() {
+        lock.lock();
+        increment();
+        lock.unlock();
+    }
+
+    public void showCounter(){
+        System.out.println(counter);
+    }
+}
